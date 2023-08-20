f24e007500675015cfd21ef19919d392ed93a48f
BaoTQ34 <baotq34@fpt.com>
2023-08-20 20:10:18 +0700

    deploy server by render

--- src/config/configReder.js
+++ src/config/configReder.js
@@ -1,3 +1,31 @@
+// db local
+// {
+//       "development": {
+//         "username": "root",
+//         "password": "trungbqph09417",
+//         "database": "thuephongtro123",
+//         "host": "127.0.0.1",
+//         "dialect": "mysql",
+//         "logging":false
+//       },
+//       "test": {
+//         "username": "root",
+//         "password": null,
+//         "database": "database_test",
+//         "host": "127.0.0.1",
+//         "dialect": "mysql"
+//       },
+//       "production": {
+//         "username": "root",
+//         "password": null,
+//         "database": "database_production",
+//         "host": "127.0.0.1",
+//         "dialect": "mysql"
+//       }
+//     }
+
+// server online
+
 // {
 //       "development": {
 //         "username": "db_phongtro123_nmkw_user",
